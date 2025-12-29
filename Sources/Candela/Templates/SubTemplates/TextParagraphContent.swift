8d6d1a0c6abdce9cd377585815749a2253059489
saket bhushan <saket.bhushan47@gmail.com>
2025-12-29 15:46:43 +0530

    Add TextParagraphContent to be used by the templates

---
+++ Sources/Candela/Templates/SubTemplates/TextParagraphContent.swift
@@ -0,0 +1,28 @@
+//
+//  TextParagraphContent.swift
+//  Candela
+//
+//  Created by saket bhushan on 29/12/25.
+//
+
+import SwiftUI
+
+public struct TextParagraphContent: View {
+    
+    @Environment(\.theme.colors) private var colors
+    @Environment(\.theme.dimensions) private var dimensions
+    let paragraph: String
+    
+    public init(paragraph: String) {
+        self.paragraph = paragraph
+    }
+    
+    public var body: some View {
+        Text(paragraph)
+// TODO:            .font(style: .body)
+            .foregroundStyle(colors.surfaceDefault)
+            .fixedSize(horizontal: false, vertical: true)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .multilineTextAlignment(.leading)
+    }
+}
